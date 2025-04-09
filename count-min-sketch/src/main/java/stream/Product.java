4ba05e06e519b1fe88d1857f747660444acb9ea4
Eric Fournier <edfournier@umass.edu>
2025-04-09 16:44:36 -0400

    switch to opencsv

--- count-min-sketch/src/main/java/stream/Product.java
+++ count-min-sketch/src/main/java/stream/Product.java
@@ -1,25 +1,14 @@
 package stream;
 
+import com.opencsv.bean.CsvBindByName;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import java.util.Objects;
 
+
 public class Product {
-    public String product_id;
-    public String product_name;
+    @CsvBindByName(column = "product_id") public String productId;
+    @CsvBindByName(column = "product_name") public String productName;
     public String category;
-    public double discounted_price;
-    public double actual_price;
-    public double discount_percentage;
-    public double rating;
-    public int rating_count;
-    public String about_product;
-    public String user_id;
-    public String user_name;
-    public String review_id;
-    public String review_title;
-    public String review_content;
-    public String img_link;
-    public String product_link;
 
     @Override
     public boolean equals(Object object) {
@@ -31,44 +20,14 @@ public class Product {
         }
         Product other = (Product)object;
         return new EqualsBuilder()
-            .append(product_id, other.product_id)
-            .append(product_name, other.product_name)
+            .append(productId, other.productId)
+            .append(productName, other.productName)
             .append(category, other.category)
-            .append(discounted_price, other.discounted_price)
-            .append(actual_price, other.actual_price)
-            .append(discount_percentage, other.discount_percentage)
-            .append(rating, other.rating)
-            .append(rating_count, other.rating_count)
-            .append(about_product, other.about_product)
-            .append(user_id, other.user_id)
-            .append(user_name, other.user_name)
-            .append(review_id, other.review_id)
-            .append(review_title, other.review_title)
-            .append(review_content, other.review_content)
-            .append(img_link, other.img_link)
-            .append(product_link, other.product_link)
             .isEquals();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(
-            product_id,
-            product_name,
-            category,
-            discounted_price,
-            actual_price,
-            discount_percentage,
-            rating,
-            rating_count,
-            about_product,
-            user_id,
-            user_name,
-            review_id,
-            review_title,
-            review_content,
-            img_link,
-            product_link
-        );
+        return Objects.hash(productId, productName, category);
     }
 }
--- count-min-sketch/src/main/java/stream/ProductIterator.java
+++ count-min-sketch/src/main/java/stream/ProductIterator.java
@@ -1,24 +1,28 @@
 package stream;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
-
 import java.io.*;
 import java.util.Iterator;
-import java.util.ArrayList;
 import java.util.List;
+import com.opencsv.bean.CsvToBeanBuilder;
 
 public class ProductIterator implements Iterator<Product>, Serializable {
 
     public ProductIterator() throws IOException {
+        // Open CSV file
         InputStream stream = getClass().getResourceAsStream("/amazon.csv");
         if (stream == null) {
-            throw new IOException("Ensure amazon.csv is in resources ");
+            throw new IOException("Missing amazon.csv in resources folder");
         }
+
+        // Parse CSV file into POJOs
         Reader in = new InputStreamReader(stream);
-        Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(in);
-        for (CSVRecord record : records) {
-            System.out.println(record);
+        List<Product> products = new CsvToBeanBuilder<Product>(in)
+            .withType(Product.class)
+            .build()
+            .parse();
+
+        for (Product product : products) {
+            System.out.println(product.productName);
         }
     }
 
