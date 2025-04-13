a6d690d44516d14392dec3f4cfd933f0b4061f90
Eric Fournier <edfournier@umass.edu>
2025-04-12 22:57:49 -0400

    move distribution logic into seperate class

---
+++ count-min-sketch/src/main/java/distribution/CustomDistribution.java
@@ -0,0 +1,51 @@
+package distribution;
+
+import org.apache.flink.configuration.YamlParserUtils;
+import java.io.File;
+import java.util.*;
+
+public class CustomDistribution implements Distribution<String> {
+    private final List<String> implicit;
+    private final HashMap<String, Double> explicit;
+    private final Random random;
+
+    public CustomDistribution() {
+        explicit = new HashMap<>();
+        implicit = new ArrayList<>();
+        random = new Random();
+    }
+
+    public CustomDistribution fromYaml(Collection<String> domain, File file) {
+        try {
+            // Load weights from config file
+            explicit.clear();
+            Map<String, Object> yaml = YamlParserUtils.loadYamlFile(file);
+            for (String category : YamlParserUtils.loadYamlFile(file).keySet()) {
+                explicit.put(category, (Double)yaml.get(category));
+            }
+
+            // Separate categories with unspecified weight
+            implicit.clear();
+            for (String category : domain) {
+                if (!explicit.containsKey(category)) {
+                    implicit.add(category);
+                }
+            }
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return this;
+    }
+
+    public String sample() {
+        double roll = random.nextDouble();
+        for (String category : explicit.keySet()) {
+            roll -= explicit.get(category);
+            if (roll <= 0) {
+                return category;
+            }
+        }
+        return implicit.get(random.nextInt(implicit.size()));
+    }
+}
--- count-min-sketch/src/main/java/stream/PurchaseGenerator.java
+++ count-min-sketch/src/main/java/stream/PurchaseGenerator.java
@@ -5,16 +5,15 @@ import java.net.URL;
 import java.util.*;
 import java.util.stream.Collectors;
 import com.opencsv.bean.CsvToBeanBuilder;
-import org.apache.flink.configuration.YamlParserUtils;
+import distribution.CustomDistribution;
+import distribution.Distribution;
 
 public class PurchaseGenerator implements Serializable {
-    private final double implicitWeight;
-    private final Map<String, Double> explicitWeights;
     private final Map<String, List<Purchase>> groups;
+    private final Distribution<String> categoryDist;
     private final Random random;
 
     public PurchaseGenerator() {
-        random = new Random();
         try {
             // Load resource files
             URL weightsUrl = getClass().getResource("/weights.yaml");
@@ -32,19 +31,10 @@ public class PurchaseGenerator implements Serializable {
                 .stream()
                 .collect(Collectors.groupingBy(Purchase::getCategory));
 
-            // Parse category weights explicitly assigned in weights.yaml
-            explicitWeights = new HashMap<>();
-            File weightsFile = new File(weightsUrl.getPath());
-            Map<String, Object> yaml = YamlParserUtils.loadYamlFile(weightsFile);
-            Double total = 0.0;
-            for (String category : yaml.keySet()) {
-                Double weight = (Double)yaml.get(category);
-                total += weight;
-                explicitWeights.put(category, weight);
-            }
-
-            // Uniform weight used for the remaining categories
-            implicitWeight = (1.0 - total) / (groups.size() - yaml.size());
+            // Setup category distribution
+            File file = new File(weightsUrl.getPath());
+            categoryDist = new CustomDistribution().fromYaml(groups.keySet(), file);
+            random = new Random();
         }
         catch (Exception e) {
             throw new RuntimeException(e);
@@ -52,16 +42,8 @@ public class PurchaseGenerator implements Serializable {
     }
 
     public Purchase next() {
-        // TODO: Most iterations just subtract the uniform weight. Faster to iterate over assigned weights, then do some static calculation if it's not one of those
-        double threshold = random.nextDouble();
-        for (String category : groups.keySet()) {
-            double weight = explicitWeights.getOrDefault(category, implicitWeight);
-            threshold -= weight;
-            if (threshold <= 0) {
-                List<Purchase> group = groups.get(category);
-                return group.get(random.nextInt(group.size()));
-            }
-        }
-        return null; // Rounding error could trigger this but not sure
+        String category = categoryDist.sample();
+        List<Purchase> group = groups.get(category);
+        return group.get(random.nextInt(group.size()));
     }
 }
